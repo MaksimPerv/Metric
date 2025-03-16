d41a10872b1e22e445412c8a20eb0d336af9c9e3
Maksim <maksimpervitskiy@MacBook-Pro-Maksim.local>
2025-03-16 19:47:29 +0300

    Реализован первый инкремент

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"github.com/MaksimPerv/Metric/internal/handlers"
+	"github.com/MaksimPerv/Metric/internal/storage"
+	"net/http"
+)
+
+func main() {
+	run()
+}
+
+func run() error {
+
+	storage := storage.NewMemStorage()
+
+	handler := hendlers.NewMetricsHandlers(storage)
+
+	http.HandleFunc("/", handler.UpdateMetric)
+	//http.HandleFunc("/get", handler.GetMetrics)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		return err
+	}
+	return nil
+}
