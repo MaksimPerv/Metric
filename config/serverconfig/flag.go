8053b9250bbdc0e54a831c7af1fc70eb910ed02c
maksimpervitskiy <maksim_perv@mail.ru>
2025-03-25 12:42:17 +0300

    iter4 проверкаq plz ww2 123 plz ww

--- config/serverconfig/flag.go
+++ config/serverconfig/flag.go
@@ -1,11 +1,17 @@
+// config/serverconfig.go
 package serverconfig
 
-import "flag"
+import (
+	"flag"
+	"strings"
+)
 
 var FlagRunAddr string
 
 func ParseFlags() {
-	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
-
+	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "server address")
 	flag.Parse()
+
+	// Убедимся, что нет http:// в начале
+	FlagRunAddr = strings.TrimPrefix(FlagRunAddr, "http://")
 }
