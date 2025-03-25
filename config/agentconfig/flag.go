bf20a696e7ed3a10ecfb039a6007f02705943e6c
maksimpervitskiy <maksim_perv@mail.ru>
2025-03-25 12:21:36 +0300

    iter4 проверкаq plz ww2 123 plz

--- config/agentconfig/flag.go
+++ config/agentconfig/flag.go
@@ -10,8 +10,12 @@ var ReportInterval time.Duration
 var PollInterval time.Duration
 
 func ParseFlags() {
+	var reportIntervalStr string
+	var pollIntervalStr string
 	flag.StringVar(&FlagRunAddr, "a", "http://localhost:8080", "address and port to run server")
-	flag.DurationVar(&ReportInterval, "r", 10*time.Second, "reporting interval (e.g. 30s, 5m)")
-	flag.DurationVar(&PollInterval, "p", 2*time.Second, "pooll")
+	flag.StringVar(&reportIntervalStr, "r", "10", "reporting interval (e.g. 30s, 5m)")
+	flag.StringVar(&pollIntervalStr, "p", "2", "pooll")
 	flag.Parse()
+	ReportInterval, _ = time.ParseDuration(reportIntervalStr)
+	PollInterval, _ = time.ParseDuration(pollIntervalStr)
 }
